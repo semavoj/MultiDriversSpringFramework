2b83eb653e001fe295cc17d5f00c6eab207454f1
Marinko Vojnic <m_vojnic@hotmail.com>
2016-05-03 16:06:29 +0200

    new password and username

--- src/main/java/utility/Constant.java
+++ src/main/java/utility/Constant.java
@@ -7,9 +7,9 @@ public class Constant {
 
     public static final String URL = "http://www.store.demoqa.com";
 
-    public static final String Username = "testuser_1";
+    public static final String Username = "b_test";
 
-    public static final String Password = "Test@123";
+    public static final String Password = "MEk2Fip#Fpoh6h#g";
 
     public static final String Path_TestData = "C://Users//Marinko//IdeaProjects//GitTest//src//main//java//testData//";
 
