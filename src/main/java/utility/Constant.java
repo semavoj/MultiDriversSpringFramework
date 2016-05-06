9d17428088d4fe64c2f57c40a4fd0dc700f05d7c
Marinko Vojnic <m_vojnic@hotmail.com>
2016-05-06 19:48:50 +0200

    new constants

--- src/main/java/utility/Constant.java
+++ src/main/java/utility/Constant.java
@@ -7,15 +7,13 @@ public class Constant {
 
     public static final String URL = "http://www.store.demoqa.com";
 
-    public static final String Username = "b_test";
+    public static final String Username = "testuser_1";
 
-    public static final String Password = "MEk2Fip#Fpoh6h#g";
+    public static final String Password ="Test@123";
 
     public static final String Path_TestData = "C:\\Users\\Marinko\\IdeaProjects\\GitTest\\src\\main\\resources\\";
 
     public static final String File_TestData = "TestData.xlsx";
-
-    public static final String SheetName = "Blad1";
 
     //Test Data Sheet Columns
 
@@ -26,4 +24,27 @@ public class Constant {
     public static final int Col_Password = 2;
 
     public static final int Col_Browser = 3;
+
+    public static final int Col_ProductType = 4;
+
+    public static final int Col_ProductNumber = 5;
+
+    public static final int Col_FirstName = 6;
+
+    public static final int Col_LastName = 7;
+
+    public static final int Col_Address = 8;
+
+    public static final int Col_City = 9;
+
+    public static final int Col_Country = 10;
+
+    public static final int Col_Phone = 11;
+
+    public static final int Col_Email = 12;
+
+    public static final int Col_Result = 13;
+
+    public static final String Path_ScreenShot = "C:\\Users\\Marinko\\IdeaProjects\\GitTest\\Screenshots";
+
 }
