0f31e7113ee9dd2aee013a70474357a11ac8ab5c
Marinko Vojnic <m_vojnic@hotmail.com>
2016-05-06 19:55:52 +0200

    changed sheet name in constants

--- src/main/java/utility/Constant.java
+++ src/main/java/utility/Constant.java
@@ -7,13 +7,15 @@ public class Constant {
 
     public static final String URL = "http://www.store.demoqa.com";
 
-    public static final String Username = "testuser_1";
+    public static final String Username = "b_test";
 
-    public static final String Password ="Test@123";
+    public static final String Password = "MEk2Fip#Fpoh6h#g";
 
     public static final String Path_TestData = "C:\\Users\\Marinko\\IdeaProjects\\GitTest\\src\\main\\resources\\";
 
     public static final String File_TestData = "TestData.xlsx";
+
+    public static final String SheetName = "Blad1";
 
     //Test Data Sheet Columns
 
